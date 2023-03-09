7b15afb38b12d2cc44c620634f8566c1cabf0d91
hello-world-99 <69188993+hello-world-99@users.noreply.github.com>
2023-03-09 17:51:35 +0300

    3

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -4,32 +4,18 @@ import org.junit.Assert.*
 
 class MainKtTest {
 
+
     @Test
-    fun transactionWrong1() {
-        val type = "Mastercard"
-        val sum = 1000.0
-        val pay = 1000.0
-        val result=transaction(type, sum, pay)
-        assertEquals(2100.0,result,0.000001)
-    }
-    @Test
-    fun transactionTrue1() {
+    fun transaction1() {
         val type = "Mastercard"
         val sum = 1000.0
         val pay = 1000.0
         val result=transaction(type, sum, pay)
         assertEquals(2000.0,result,0.000001)
     }
+
     @Test
-    fun transactionWrongC1() {
-        val type = "Maestro"
-        val sum = 1000.0
-        val pay = 100000.0
-        val result=transaction(type, sum, pay)
-        assertEquals(100000.0,result,0.000001)
-    }
-    @Test
-    fun transactionTrueC1() {
+    fun transaction2() {
         val type = "Maestro"
         val sum = 100000.0
         val pay = 1000.0
@@ -37,34 +23,60 @@ class MainKtTest {
         assertEquals(101026.0,result,0.000001)
     }
     @Test
-    fun transactionTrueC21() {
+    fun transaction8() {
+        val type = "Maestro"
+        val sum = 1000.0
+        val pay = 100000.0
+        val result=transaction(type, sum, pay)
+        assertEquals(101620.0,result,0.000001)
+    }
+    @Test
+    fun transaction3() {
         val type = "Visa"
         val sum = 100.0
         val pay = 1000.0
         val result=transaction(type, sum, pay)
         assertEquals(1107.5,result,0.000001)
     }
+
     @Test
-    fun transactionWrongC21() {
-        val type = "Visa"
+    fun transaction4() {
+        val type = "Мир"
+        val sum = 100.0
+        val pay = 1000.0
+        val result=transaction(type, sum, pay)
+        assertEquals(1107.5,result,0.000001)
+    }
+    @Test
+    fun transaction5() {
+        val type = "Мир"
+        val sum = 20.0
+        val pay = 5.0
+        val result=transaction(type, sum, pay)
+        assertEquals(0.0,result,0.000001)
+    }
+
+    @Test
+    fun transaction6() {
+        val type = "VK Pay"
         val sum = 20.0
         val pay = 5.0
         val result=transaction(type, sum, pay)
         assertEquals(25.0,result,0.000001)
     }
     @Test
-    fun transactionTrueC22() {
-        val type = "Мир"
-        val sum = 100.0
-        val pay = 1000.0
+    fun transaction7() {
+        val type = "231"
+        val sum = -1.0
+        val pay = 0.0
         val result=transaction(type, sum, pay)
-        assertEquals(1107.5,result,0.000001)
+        assertEquals(0.0,result,0.000001)
     }
     @Test
-    fun transactionWrongC22() {
-        val type = "Мир"
-        val sum = 20.0
-        val pay = 5.0
+    fun transaction9() {
+        val type = ""
+        val sum = -1.0
+        val pay = 0.0
         val result=transaction(type, sum, pay)
         assertEquals(0.0,result,0.000001)
     }
